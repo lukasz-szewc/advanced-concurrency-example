a5587d01c18b11131f77cc10d94c753fc1e60f46
lukasz-szewc <lukasz-szewc@users.noreply.github.com>
2016-04-24 12:01:12 +0200

    changed implementation of interrupting threads app

--- src/main/java/de/concurrency/multithreading/interruption/InterruptionApp.java
+++ src/main/java/de/concurrency/multithreading/interruption/InterruptionApp.java
@@ -3,38 +3,24 @@ package de.concurrency.multithreading.interruption;
 public class InterruptionApp {
 
     public static void main(String[] args) {
-
-        Object lock = new Object();
-
-        CountingThread countingThread = new CountingThread(lock);
+        CountingThread countingThread = new CountingThread();
         countingThread.start();
 
         while (true) {
-            synchronized (lock) {
-                if (countingThread.getI() >= 7) {
-                    countingThread.interrupt();
-                    break;
-                }
+            if (countingThread.getI() >= 7) {
+                countingThread.interrupt();
+                break;
             }
         }
-
-
-    }
-}
-
-class CountingThread extends Thread {
-
-    private volatile int i = 0;
-    private final Object lock;
-
-    CountingThread(Object lock) {
-        this.lock = lock;
     }
 
-    @Override
-    public void run() {
-        while (true) {
-            synchronized (lock) {
+    static class CountingThread extends Thread {
+
+        private volatile int i = 0;
+
+        @Override
+        public void run() {
+            while (true) {
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException ex) {
@@ -42,13 +28,13 @@ class CountingThread extends Thread {
                     break;
                 }
                 System.out.println("increment i: " + ++i);
-                lock.notify();
             }
+
         }
 
-    }
+        public int getI() {
+            return i;
+        }
 
-    public int getI() {
-        return i;
     }
 }
